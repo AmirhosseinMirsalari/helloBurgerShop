fac17a8d2c41f0c960805142bd3f7048e848f372
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-03-01 02:06:16 +0330

    fetch to backend with getserverside method

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,35 @@
 import Features from "@/components/Features";
+import axios from "axios";
+import { handleError } from "lib/helper";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
-const Home = () => {
+const Home = ({ productsTab, error }) => {
+  useEffect(() => {
+    error && toast.error(error)
+  }, [error])
+
   return (
     <Features />
   )
 }
 
 export default Home;
+
+export async function getServerSideProps() {
+  try {
+    const res = await axios.get("/products/products-tabs")
+    console.log(res.data.data);
+    return {
+      props: {
+        productsTab: res.data.data
+      }
+    }
+  } catch (err) {
+    return {
+      props: {
+        error: handleError(err)
+      }
+    }
+  }
+}
