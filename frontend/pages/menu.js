f58e8c6752e575be7fcb71d4d15ea43c3b3b3a87
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-03-01 19:45:37 +0330

    get products from backend

--- frontend/pages/menu.js
+++ frontend/pages/menu.js
@@ -2,8 +2,9 @@ import { handleError } from "lib/helper";
 import { useEffect } from "react";
 import { toast } from "react-toastify"
 import axios from "axios"
+import Product from "@/components/product/Product";
 
-const MenuPage = ({ categories, error }) => {
+const MenuPage = ({ products, categories, error }) => {
     useEffect(() => {
         error && toast.error(error)
     }, [error])
@@ -40,28 +41,28 @@ const MenuPage = ({ categories, error }) => {
                             <label className="form-label">مرتب سازی</label>
                             <div className="form-check my-2">
                                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                                <label className="form-check-label cursor-pointer" for="flexRadioDefault1">
+                                <label className="form-check-label cursor-pointer" htmlFor="flexRadioDefault1">
                                     بیشترین قیمت
                                 </label>
                             </div>
                             <div className="form-check my-2">
                                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
-                                    checked />
-                                <label className="form-check-label cursor-pointer" for="flexRadioDefault2">
+                                />
+                                <label className="form-check-label cursor-pointer" htmlFor="flexRadioDefault2">
                                     کمترین قیمت
                                 </label>
                             </div>
                             <div className="form-check my-2">
                                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"
-                                    checked />
-                                <label className="form-check-label cursor-pointer" for="flexRadioDefault3">
+                                />
+                                <label className="form-check-label cursor-pointer" htmlFor="flexRadioDefault3">
                                     پرفروش ترین
                                 </label>
                             </div>
                             <div className="form-check my-2">
                                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4"
-                                    checked />
-                                <label className="form-check-label cursor-pointer" for="flexRadioDefault4">
+                                />
+                                <label className="form-check-label cursor-pointer" htmlFor="flexRadioDefault4">
                                     با تخفیف
                                 </label>
                             </div>
@@ -69,42 +70,19 @@ const MenuPage = ({ categories, error }) => {
                     </div>
                     <div className="col-sm-12 col-lg-9">
                         <div className="row gx-3">
-                            <div className="col-sm-6 col-lg-4">
-                                <div className="box">
-                                    <div>
-                                        <div className="img-box">
-                                            <img className="img-fluid" src="./images/b1.jpg" alt="" />
-                                        </div>
-                                        <div className="detail-box">
-                                            <h5>
-                                                لورم ایپسوم متن
-                                            </h5>
-                                            <p>
-                                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                از
-                                                طراحان
-                                                گرافیک است.
-                                            </p>
-                                            <div className="options">
-                                                <h6>
-                                                    <del>45,000</del>
-                                                    34,000
-                                                    <span>تومان</span>
-                                                </h6>
-                                                <a href="">
-                                                    <i className="bi bi-cart-fill text-white fs-5"></i>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
+                            {products && products.products.map((product, index) => (
+                                <div key={index} className="col-sm-6 col-lg-4">
+                                    <Product product={product} />
                                 </div>
-                            </div>
+                            ))}
                         </div>
                         <nav className="d-flex justify-content-center mt-5">
                             <ul className="pagination">
-                                <li className="page-item active"><a className="page-link" href="#">1</a></li>
-                                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                {products && products.meta.links.slice(1, -1).map((link, index) => (
+                                    <li key={index} className={link.active ? 'page-item active' : 'page-item'}>
+                                        <button className="page-link">{link.label}</button>
+                                    </li>
+                                ))}
                             </ul>
                         </nav>
                     </div>
@@ -118,10 +96,12 @@ export default MenuPage;
 
 export async function getServerSideProps() {
     try {
+        const res = await axios.get("/menu")
         const resCate = await axios.get("/categories")
         // console.log(res.data.data);
         return {
             props: {
+                products: res.data.data,
                 categories: resCate.data.data
             }
         }
