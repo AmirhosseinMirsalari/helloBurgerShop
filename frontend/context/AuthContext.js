075ab51f24acf2c4cccc07b5cf2f8bf4b3e9efd0
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-03-02 17:05:24 +0330

    fetching to backend for login and set cookie

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -1,11 +1,28 @@
-const { createContext } = require("react");
+import { toast } from "react-toastify";
+import { handleError } from "lib/helper";
+import { createContext, useState } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false)
 
-    const login = async () => {
-        console.log('Login AuthContext');
+    const login = async (cellphone) => {
+        try {
+            setLoading(true)
+
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_APP_API_URL}/auth/login`, {
+                cellphone
+            })
+
+            toast.success(res.data.message);
+
+        } catch (err) {
+            toast.error(handleError(err))
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
