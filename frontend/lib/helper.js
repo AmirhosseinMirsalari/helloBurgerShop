108dc0e7ca232ec9c76e248aa3cdffdda89ef85a
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-03-03 14:12:03 +0330

    save cart in localStorage

--- frontend/lib/helper.js
+++ frontend/lib/helper.js
@@ -26,4 +26,20 @@ const handleError = (err) => {
     // return 'خطای سرور، دوباره تلاش کنید'
 }
 
-export { handleError,numberFormat }
+const saveStorage = (cart) => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("shopping-cart", JSON.stringify(cart));
+    }
+  };
+  
+  const getStorage = () => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("shopping-cart")
+        ? JSON.parse(localStorage.getItem("shopping-cart"))
+        : [];
+    } else {
+      return [];
+    }
+  };
+
+export { handleError,numberFormat,saveStorage,getStorage }
--- frontend/redux/shoppingCart/cartSlice.js
+++ frontend/redux/shoppingCart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import { getStorage, saveStorage } from "lib/helper";
 
 const initialState = {
-  cart: []
+  cart: getStorage(),
 };
 
 const cartSlice = createSlice({
@@ -14,23 +15,29 @@ const cartSlice = createSlice({
         ...state.cart,
         { ...action.payload.product, qty: action.payload.qty },
       ];
+      saveStorage(state.cart)
       toast.success("محصول با موفقیت اضافه شد");
     },
     removeFromCart: (state, action) => {
       state.cart = state.cart.filter((p) => p.id !== action.payload.id);
+      saveStorage(state.cart)
     },
     increment: (state, action) => {
       state.cart = state.cart.map((p) =>
         p.id === action.payload ? { ...p, qty: p.qty + 1 } : p
       );
+      saveStorage(state.cart)
     },
     decrement: (state, action) => {
       state.cart = state.cart.map((p) =>
         p.id === action.payload ? { ...p, qty: p.qty - 1 } : p
       );
+      saveStorage(state.cart)
+
     },
     clearCart: (state, action) => {
       state.cart = [];
+      saveStorage(state.cart)
     },
     
   },
