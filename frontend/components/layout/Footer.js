975a12aa5128b4af63d00c308cb474328e12a0bd
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-06 06:49:31 +0330

    add social media link to the icons footer

--- frontend/components/layout/Footer.js
+++ frontend/components/layout/Footer.js
@@ -1,84 +1,60 @@
 const Footer = () => {
-    return (
-        <footer className="footer_section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4 footer-col">
-                        <div className="footer_contact">
-                            <h4>
-                                تماس با ما
-                            </h4>
-                            <div className="contact_link_box">
-                                <a href="">
-                                    <i className="bi bi-geo-alt-fill"></i>
-                                    <span>
-                                        آدرس
-                                    </span>
-                                </a>
-                                <a href="">
-                                    <div className="d-flex justify-content-center">
-                                        <i className="bi bi-telephone-fill" aria-hidden="true"></i>
-                                        <p className="my-0" style={{ direction: 'ltr' }}>
-                                            0910 000 0000
-                                        </p>
-                                    </div>
-                                </a>
-                                <a href="">
-                                    <i className="bi bi-envelope-fill"></i>
-                                    <span>
-                                        demo@gmail.com
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 footer-col">
-                        <div className="footer_detail">
-                            <a href="" className="footer-logo">
-                                
-                            </a>
-                            <p>
-                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-                            </p>
-                            <div className="footer_social">
-                                <a href="">
-                                    <i className="bi bi-facebook"></i>
-                                </a>
-                                <a href="">
-                                    <i className="bi bi-twitter"></i>
-                                </a>
-                                <a href="">
-                                    <i className="bi bi-linkedin"></i>
-                                </a>
-                                <a href="">
-                                    <i className="bi bi-instagram"></i>
-                                </a>
-                                <a href="">
-                                    <i className="bi bi-pinterest"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 footer-col">
-                        <h4>
-                            ساعت کاری
-                        </h4>
-                        <p>
-                            هر روز
-                        </p>
-                        <p>
-                            12.00 صبح تا 12.00 شب
-                        </p>
-                    </div>
-                </div>
-                <div className="footer-info">
-                    <p>
-                        لورم ایپسوم متن ساختگی با تولید سادگی
+  return (
+    <footer className="footer_section">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 footer-col">
+            <div className="footer_contact">
+              <h4>تماس با ما</h4>
+              <div className="contact_link_box">
+                <a href="">
+                  <div className="d-flex justify-content-center">
+                    <p className="my-0" style={{ direction: "ltr" }}>
+                      0933 2800 956
                     </p>
-                </div>
+                    <i
+                      className="bi bi-telephone-fill mx-2"
+                      aria-hidden="true"
+                    ></i>
+                  </div>
+                </a>
+                <a href="">
+                  <span>amirhossein.mir98@gmail.com</span>
+                  <i className="bi bi-envelope-fill m-2"></i>
+                </a>
+              </div>
             </div>
-        </footer>
-    )
-}
+          </div>
+          <div className="col-md-4 footer-col">
+            <div className="footer_detail">
+              <a href="" className="footer-logo"></a>
+              <p>
+                هلو برگر توسط فریمورک Nextjs و به کمک Bootstrap 5 و با سعی بر کاهش تعداد کتابخانه ها برای سبک بودن پروژه، نوشته شده است
+              </p>
+              <div className="footer_social">
+                <a href="https://twitter.com/Amirhossein_ms8">
+                  <i className="bi bi-twitter"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/amirhossein-mirsalari-49a54b1ab/?originalSubdomain=ir">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a href="https://github.com/AmirhosseinMirsalari">
+                  <i className="bi bi-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 footer-col">
+            <h4>ساعت کاری</h4>
+            <p> هر روز هفته، از ۱۲ ظهر تا ۱۲ شب</p>
+          </div>
+        </div>
+        <div className="footer-info">
+          <p>برنامه نویس: امیرحسین میرسالاری</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
