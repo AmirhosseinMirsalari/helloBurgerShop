16f8a477e75594dcd5367c4cc56342be13a6f5c7
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-06 06:44:37 +0330

    add new map in contact form and set new design to the form

--- frontend/components/contact/ContactForm.js
+++ frontend/components/contact/ContactForm.js
@@ -4,58 +4,103 @@ import { toast } from "react-toastify";
 import axios from "axios";
 
 const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [text, setText] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (name === '', email === '', subject === '', text === '') {
-            toast.error("تمام موارد فرم تماس با ما الزامی است.")
-            return
-        }
-
-        try {
-            setLoading(true)
-            const res = await axios.post("/contact-us", {
-                name,
-                email,
-                subject,
-                text
-            })
-            toast.success('پیام شما ثبت شد');
-        } catch (err) {
-            toast.error(handleError(err))
-        } finally {
-            setLoading(false)
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ((name === "", email === "", subject === "", text === "")) {
+      toast.error("تمام موارد فرم تماس با ما الزامی است.");
+      return;
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <input onChange={(e) => setName(e.target.value)} value={name} type="text" className="form-control" placeholder="نام و نام خانوادگی" />
-            </div>
-            <div>
-                <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className="form-control" placeholder="ایمیل" />
-            </div>
-            <div>
-                <input onChange={(e) => setSubject(e.target.value)} value={subject} type="text" className="form-control" placeholder="موضوع پیام" />
-            </div>
-            <div>
-                <textarea onChange={(e) => setText(e.target.value)} value={text} rows="10" style={{ height: '100px' }} className="form-control"
-                    placeholder="متن پیام"></textarea>
-            </div>
-            <div className="btn_box">
-                <button type="submit" disabled={loading}>
-                    ارسال پیام
-                    {loading && <div className="spinner-border spinner-border-sm ms-2"></div>}
-                </button>
-            </div>
-        </form>
-    )
-}
+    try {
+      setLoading(true);
+      const res = await axios.post("/contact-us", {
+        name,
+        email,
+        subject,
+        text,
+      });
+      toast.success("پیام شما ثبت شد");
+      setName("")
+      setEmail("")
+      setSubject("")
+      setText("")
+
+    } catch (err) {
+      toast.error(handleError(err));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="bg-light p-5">
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label fw-bold mb-2">
+          نام
+        </label>
+        <input
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          type="text"
+          className="form-control"
+          placeholder="نام و نام خانوادگی"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label fw-bold mb-2">
+          ایمیل
+        </label>
+        <input
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          className="form-control"
+          placeholder="Email"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="subject" className="form-label fw-bold mb-2">
+          موضوع پیام
+        </label>
+        <input
+          id="subject"
+          onChange={(e) => setSubject(e.target.value)}
+          value={subject}
+          type="text"
+          className="form-control"
+          placeholder="موضوع پیام"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="message" className="form-label fw-bold mb-2">
+          متن پیام
+        </label>
+        <textarea
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          rows="10"
+          style={{ height: "100px" }}
+          id="message"
+          className="form-control"
+          placeholder="متن پیام خود را وارد کنید"
+        ></textarea>{" "}
+      </div>
+      <button type="submit" disabled={loading}>
+        ارسال پیام
+        {loading && (
+          <div className="spinner-border spinner-border-sm ms-2"></div>
+        )}
+      </button>{" "}
+    </form>
+  );
+};
 
 export default ContactForm;
--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -1,4 +1,5 @@
 import Map from "@/components/Map";
+import Testimonial from "@/components/Testimonial";
 import ContactForm from "@/components/contact/ContactForm";
 import Head from "next/head";
 
@@ -8,40 +9,48 @@ const ContactPage = () => {
 
   return (
     <section className="book_section layout_padding">
-      <Head>
-        <title>ارتباط با ما</title>
-      </Head>
-      <div className="container">
-        <div className="heading_container">
-          <h2>تماس با ما</h2>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="form_container">
-              <ContactForm />
+        <Head>
+  <title>ارتباط با ما</title>
+</Head>
+        <div className="container">
+          <div className="heading_container">
+            <h3 className=" fw-bold mb-2 ">منتظر شنیدن نظرات شما هستیم</h3>
+          </div>
+          <div style={{paddingBottom:"100px"}} className="bg-light px-3 border-0 rounded shadow shadow-hover">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form_container">
+                  <ContactForm />
+                </div>
+              </div>
+              <div className="col-md-6 d-flex align-items-center">
+                <div className="map_container ">
+                  <div class="bg-light p-3 border-0 rounded shadow shadow-hover m-auto">
+                    <Map
+                      width="800"
+                      height="400"
+                      center={DEFAULT_CENTER}
+                      zoom={15}
+                    >
+                      {({ TileLayer, Marker, Popup }) => (
+                        <>
+                          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                          <Marker position={DEFAULT_CENTER}>
+                            <Popup>
+                              ما اینجاییم، حضورا هم درخدمت شما هستیم
+                            </Popup>
+                          </Marker>
+                        </>
+                      )}
+                    </Map>
+                  </div>
+                </div>
+              </div>
+              <Testimonial />
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="map_container ">
-            <Map width="800" height="400" center={DEFAULT_CENTER} zoom={15}>
-                  {({ TileLayer, Marker, Popup }) => (
-                    <>
-                      <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                      />
-                      <Marker position={DEFAULT_CENTER}>
-                        <Popup>
-                          ما اینجاییم، حضورا هم درخدمت شما هستیم
-                        </Popup>
-                      </Marker>
-                    </>
-                  )}
-                </Map>
-            </div>
-          </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 };
 
