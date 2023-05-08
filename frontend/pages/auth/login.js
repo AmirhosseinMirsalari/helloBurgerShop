9033778a41b37f852149f4ee660d3bfbd0e8e831
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-08 04:07:05 +0330

    add placeholder in phoneNumber input and fix style in this page

--- frontend/pages/auth/login.js
+++ frontend/pages/auth/login.js
@@ -1,21 +1,33 @@
 import CheckOtp from "@/components/auth/CheckOtp";
 import Login from "@/components/auth/Login";
 import Head from "next/head";
+import Image from "next/image";
 import { useState } from "react";
+import logo from "../../public/images/logo.png"
+import { useRouter } from "next/router";
+import { useIsMobile } from "lib/helper";
 
 const LoginPage = () => {
   const [setp, setStep] = useState(1);
+  const router = useRouter()
+  const isMobile = useIsMobile()
 
   return (
     <section className="auth_section book_section">
       <Head>
         <title>ورود به حساب کاربری</title>
       </Head>
-      <div className="container">
+      <div className="container ">
         <div className="row mt-5">
           <div className="col-md-4 offset-md-4">
-            <div className="card">
+            <div className={`card shadow shadow-hover ${isMobile && "mx-4"}`} style={{marginTop:"20%"}}>
               <div className="card-body">
+                <div className="cursor-pointer">
+                <Image onClick={()=>router.push("/")} src={logo}   />
+
+                </div>
+              <h5 className=" fw-bold mb-5 text-center">ورود به حساب کاربری</h5>
+
                 {setp === 1 && <Login setStep={setStep} />}
                 {setp === 2 && <CheckOtp />}
               </div>
