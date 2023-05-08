0daeb405b819897685c70bac0775bd500ae39c6f
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-08 04:04:16 +0330

    add responsive menu in mobile device

--- frontend/pages/menu.js
+++ frontend/pages/menu.js
@@ -1,10 +1,11 @@
-import { handleError } from "lib/helper";
+import { handleError, useIsMobile } from "lib/helper";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
 import Product from "@/components/product/Product";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Link from "next/link";
 
 const MenuPage = ({ products, categories, error }) => {
   const [search, setSearch] = useState("");
@@ -12,6 +13,7 @@ const MenuPage = ({ products, categories, error }) => {
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     error && toast.error(error);
@@ -61,7 +63,10 @@ const MenuPage = ({ products, categories, error }) => {
   };
 
   return (
-    <section className="food_section layout_padding">
+    <section
+      style={{ backgroundColor: "rgb(245 245 244)" }}
+      className="food_section layout_padding"
+    >
       <Head>
         <title>منوی غذا</title>
       </Head>
@@ -69,14 +74,172 @@ const MenuPage = ({ products, categories, error }) => {
         <div className="row">
           <div className="col-sm-12 col-lg-3">
             <div>
-              <label className="form-label">جستجو</label>
+              {isMobile && (
+                <div>
+                  <div className="d-flex justify-content-between">
+                    <div
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#offcanvasMenu"
+                      aria-controls="offcanvasMenu"
+                      aria-label="Toggle navigation"
+                      className="d-inline-block"
+                    >
+                      <i className="bi bi-filter"></i>
+                      <span className="mx-2 cursor-pointer">فیلتر محصولات</span>
+                    </div>
+                    <div onClick={() => router.push("/menu")}>
+                    <i style={{color:"red"}} className="bi bi-x-lg"></i>
+                      <span className="mx-1 cursor-pointer">حذف فیلتر ها</span>
+                    </div>
+                  </div>
+                  <div
+                    className="offcanvas offcanvas-end"
+                    tabIndex="-1"
+                    style={{ backgroundColor: "rgb(245 245 244)" }}
+                    id="offcanvasMenu"
+                    aria-labelledby="offcanvasMenuLabel"
+                  >
+                    <div
+                      style={{ margin: "0 3px -10px 3px" }}
+                      className="offcanvas-header"
+                    >
+                      <h5 className="offcanvas-title" id="offcanvasMenuLabel">
+                        فیلتر محصولات
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="offcanvas"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="mx-4">
+                      <hr />
+                      <div className="filter-list shadow">
+                        <div className="form-label ">دسته بندی</div>
+                        <ul className="mt-3">
+                          {categories &&
+                            categories.map((category, index) => (
+                              <li
+                                onClick={() =>
+                                  handleFilter({ category: category.id })
+                                }
+                                key={index}
+                                className={
+                                  router.query.hasOwnProperty("category") &&
+                                  router.query.category == category.id
+                                    ? "my-2 cursor-pointer filter-list-active"
+                                    : "my-2 cursor-pointer"
+                                }
+                              >
+                                {category.name}
+                              </li>
+                            ))}
+                        </ul>
+                      </div>
+                      <hr />
+                      <div className="filter-list shadow">
+                        <label className="form-label">مرتب سازی</label>
+                        <div className="form-check my-2">
+                          <input
+                            onChange={() => handleFilter({ sortBy: "max" })}
+                            checked={
+                              router.query.hasOwnProperty("sortBy") &&
+                              router.query.sortBy == "max"
+                            }
+                            className="form-check-input"
+                            type="radio"
+                            name="flexRadioDefault"
+                            id="flexRadioDefault1"
+                          />
+                          <label
+                            className="form-check-label cursor-pointer"
+                            htmlFor="flexRadioDefault1"
+                          >
+                            بیشترین قیمت
+                          </label>
+                        </div>
+                        <div className="form-check my-2">
+                          <input
+                            onChange={() => handleFilter({ sortBy: "min" })}
+                            checked={
+                              router.query.hasOwnProperty("sortBy") &&
+                              router.query.sortBy == "min"
+                            }
+                            className="form-check-input"
+                            type="radio"
+                            name="flexRadioDefault"
+                            id="flexRadioDefault2"
+                          />
+                          <label
+                            className="form-check-label cursor-pointer"
+                            htmlFor="flexRadioDefault2"
+                          >
+                            کمترین قیمت
+                          </label>
+                        </div>
+                        <div className="form-check my-2">
+                          <input
+                            onChange={() =>
+                              handleFilter({ sortBy: "bestseller" })
+                            }
+                            checked={
+                              router.query.hasOwnProperty("sortBy") &&
+                              router.query.sortBy == "bestseller"
+                            }
+                            className="form-check-input"
+                            type="radio"
+                            name="flexRadioDefault"
+                            id="flexRadioDefault3"
+                          />
+                          <label
+                            className="form-check-label cursor-pointer"
+                            htmlFor="flexRadioDefault3"
+                          >
+                            پرفروش ترین
+                          </label>
+                        </div>
+                        <div className="form-check my-2">
+                          <input
+                            onChange={() => handleFilter({ sortBy: "sale" })}
+                            checked={
+                              router.query.hasOwnProperty("sortBy") &&
+                              router.query.sortBy == "sale"
+                            }
+                            className="form-check-input"
+                            type="radio"
+                            name="flexRadioDefault"
+                            id="flexRadioDefault4"
+                          />
+                          <label
+                            className="form-check-label cursor-pointer"
+                            htmlFor="flexRadioDefault4"
+                          >
+                            با تخفیف
+                          </label>
+                        </div>
+                      </div>
+                      <button
+                        onClick={() => router.push("/menu")}
+                        className="btn btn-outline-secondary mt-4"
+                        type="button"
+                        data-bs-dismiss="offcanvas"
+                        aria-label="Close"
+                        style={{ padding: "8px 12px", width: "100%" }}
+                      >
+                        پاک کردن فیلترها
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
 
-              <div className="input-group mb-3">
+              <div className="input-group mt-3">
                 <input
                   onChange={(e) => setSearch(e.target.value)}
                   type="text"
                   className="form-control"
-                  placeholder="نام محصول ..."
+                  placeholder="جستجوی محصول ..."
                   aria-label="Recipient's username"
                   aria-describedby="basic-addon2"
                 />
@@ -85,112 +248,120 @@ const MenuPage = ({ products, categories, error }) => {
                   className="btn btn-outline-secondary"
                   type="button"
                   id="basic-addon2"
+                  style={{ padding: "8px 12px", width: "37%" }}
                 >
                   <i className="bi bi-search"></i> جستجو
                 </button>
               </div>
             </div>
-            <hr />
-            <div className="filter-list">
-              <div className="form-label">دسته بندی</div>
-              <ul>
-                {categories &&
-                  categories.map((category, index) => (
-                    <li
-                      onClick={() => handleFilter({ category: category.id })}
-                      key={index}
-                      className={
-                        router.query.hasOwnProperty("category") &&
-                        router.query.category == category.id
-                          ? "my-2 cursor-pointer filter-list-active"
-                          : "my-2 cursor-pointer"
+
+            {!isMobile && (
+              <>
+                <hr />
+                <div className="filter-list shadow">
+                  <div className="form-label">دسته بندی</div>
+                  <ul>
+                    {categories &&
+                      categories.map((category, index) => (
+                        <li
+                          onClick={() =>
+                            handleFilter({ category: category.id })
+                          }
+                          key={index}
+                          className={
+                            router.query.hasOwnProperty("category") &&
+                            router.query.category == category.id
+                              ? "my-2 cursor-pointer filter-list-active"
+                              : "my-2 cursor-pointer"
+                          }
+                        >
+                          {category.name}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+                <hr />
+                <div className="filter-list shadow">
+                  <label className="form-label">مرتب سازی</label>
+                  <div className="form-check my-2">
+                    <input
+                      onChange={() => handleFilter({ sortBy: "max" })}
+                      checked={
+                        router.query.hasOwnProperty("sortBy") &&
+                        router.query.sortBy == "max"
                       }
+                      className="form-check-input"
+                      type="radio"
+                      name="flexRadioDefault"
+                      id="flexRadioDefault1"
+                    />
+                    <label
+                      className="form-check-label cursor-pointer"
+                      htmlFor="flexRadioDefault1"
                     >
-                      {category.name}
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <hr />
-            <div>
-              <label className="form-label">مرتب سازی</label>
-              <div className="form-check my-2">
-                <input
-                  onChange={() => handleFilter({ sortBy: "max" })}
-                  checked={
-                    router.query.hasOwnProperty("sortBy") &&
-                    router.query.sortBy == "max"
-                  }
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault1"
-                />
-                <label
-                  className="form-check-label cursor-pointer"
-                  htmlFor="flexRadioDefault1"
-                >
-                  بیشترین قیمت
-                </label>
-              </div>
-              <div className="form-check my-2">
-                <input
-                  onChange={() => handleFilter({ sortBy: "min" })}
-                  checked={
-                    router.query.hasOwnProperty("sortBy") &&
-                    router.query.sortBy == "min"
-                  }
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault2"
-                />
-                <label
-                  className="form-check-label cursor-pointer"
-                  htmlFor="flexRadioDefault2"
-                >
-                  کمترین قیمت
-                </label>
-              </div>
-              <div className="form-check my-2">
-                <input
-                  onChange={() => handleFilter({ sortBy: "bestseller" })}
-                  checked={
-                    router.query.hasOwnProperty("sortBy") &&
-                    router.query.sortBy == "bestseller"
-                  }
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault3"
-                />
-                <label
-                  className="form-check-label cursor-pointer"
-                  htmlFor="flexRadioDefault3"
-                >
-                  پرفروش ترین
-                </label>
-              </div>
-              <div className="form-check my-2">
-                <input
-                  onChange={() => handleFilter({ sortBy: "sale" })}
-                  checked={
-                    router.query.hasOwnProperty("sortBy") &&
-                    router.query.sortBy == "sale"
-                  }
-                  className="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault4"
-                />
-                <label
-                  className="form-check-label cursor-pointer"
-                  htmlFor="flexRadioDefault4"
-                >
-                  با تخفیف
-                </label>
-              </div>
-            </div>
+                      بیشترین قیمت
+                    </label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input
+                      onChange={() => handleFilter({ sortBy: "min" })}
+                      checked={
+                        router.query.hasOwnProperty("sortBy") &&
+                        router.query.sortBy == "min"
+                      }
+                      className="form-check-input"
+                      type="radio"
+                      name="flexRadioDefault"
+                      id="flexRadioDefault2"
+                    />
+                    <label
+                      className="form-check-label cursor-pointer"
+                      htmlFor="flexRadioDefault2"
+                    >
+                      کمترین قیمت
+                    </label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input
+                      onChange={() => handleFilter({ sortBy: "bestseller" })}
+                      checked={
+                        router.query.hasOwnProperty("sortBy") &&
+                        router.query.sortBy == "bestseller"
+                      }
+                      className="form-check-input"
+                      type="radio"
+                      name="flexRadioDefault"
+                      id="flexRadioDefault3"
+                    />
+                    <label
+                      className="form-check-label cursor-pointer"
+                      htmlFor="flexRadioDefault3"
+                    >
+                      پرفروش ترین
+                    </label>
+                  </div>
+                  <div className="form-check my-2">
+                    <input
+                      onChange={() => handleFilter({ sortBy: "sale" })}
+                      checked={
+                        router.query.hasOwnProperty("sortBy") &&
+                        router.query.sortBy == "sale"
+                      }
+                      className="form-check-input"
+                      type="radio"
+                      name="flexRadioDefault"
+                      id="flexRadioDefault4"
+                    />
+                    <label
+                      className="form-check-label cursor-pointer"
+                      htmlFor="flexRadioDefault4"
+                    >
+                      با تخفیف
+                    </label>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
           {productList && productList.products.length != 0 ? (
             <>
@@ -239,7 +410,7 @@ const MenuPage = ({ products, categories, error }) => {
             </>
           ) : (
             <div className="col-sm-12 col-lg-9">
-              <div className="d-flex justify-content-center align-items-center h-100">
+              <div className="d-flex justify-content-center align-items-center h-100 mt-5">
                 <h5>محصولی یافت نشده!</h5>
               </div>
             </div>
